01d9a1bccd01ea8f03e4930f539706974569d9fe
Ariane Gomes <arianecgomes@hotmail.com>
2020-11-26 18:42:39 -0300

    Populando a tabela de Locadoras

--- db/create.sql
+++ db/create.sql
@@ -7,3 +7,7 @@ not null, cidade varchar(20) not null, email varchar(20) not null, senha varchar
 
 create table Cliente (id bigint not null auto_increment, nome varchar(30)
 not null, email varchar(20) not null, senha varchar(10) not null, primary key (id));
+
+
+insert into Locadora(nome, cidade, email, senha) values ('Loca Loka', 'Sao Carlos', 'localoka@locadora.com', 'loca');
+insert into Locadora(nome, cidade, email, senha) values ('Locadora Senna', 'Sao Paulo', 'senna@locadora.com', 'senna');

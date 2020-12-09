3515a6651e0cdc767c3f598ae794825dadcbcc90
Sabrina Miranda <sbrina.smiranda@gmail.com>
2020-12-09 14:35:50 -0300

    Update usuario.sql

--- db/usuario.sql
+++ db/usuario.sql
@@ -1,7 +1,16 @@
+create database Bicicletas;
+
 use Bicicletas;
 
-create table Usuario(id bigint not null auto_increment, nome varchar(256) not null, login varchar(20) not null unique, senha varchar(64) not null, papel varchar(10), primary key (id));
+--caso vc já tenha a tabela criada, comente o comando abaixo e use o comentado
+create table Usuario(id bigint not null auto_increment, nome varchar(256) not null, login varchar(20) not null unique, senha varchar(64) not null, papel varchar(10), cidade null varchar(30), primary key (id));
+-- alter table Usuario add cidade null varchar(30);
 
+--insere user admin
 insert into Usuario(nome, login, senha, papel) values ('Administrador', 'admin', 'admin', 'ADMIN');
 
-insert into Usuario(nome, login, senha, papel) values ('Locadora', 'user', 'user', 'LOCADORA');
+--insere user locadora
+insert into Usuario(nome, login, senha, papel) values ('LocadoraTeste', 'locadora@test', 'locadora', 'LOCADORA', 'São Carlos');
+
+--insere user cliente
+insert into Usuario(nome, login, senha, papel) values ('ClienteTeste', 'cliente@test', 'user', 'CLIENTE');
